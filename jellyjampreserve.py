7a9b60e45faa7865ea58bb1332e42b51d6056283
Beto De Almeida <roberto@dealmeida.net>
2017-05-09 18:45:00 -0700

    Update jellyjampreserve.py
    Tested and fixed script.

--- jellyjampreserve.py
+++ jellyjampreserve.py
@@ -7,32 +7,25 @@ import RPIO
 
 HOST = '127.0.0.1'
 PORT = 7133
-SWITCH = 3  # 10K pull up resistor and switch to ground
-LED = 5     # 47R resistor to ground
+SWITCH = 2
+LED = 3
 
-RPIO.setmode(RPIO.BOARD)
 RPIO.setup(LED, RPIO.OUT, initial=RPIO.LOW)
 
 
-def record(gpio_id, value):
+def callback(gpio_id, value):
     client = UDPClient(HOST, PORT)
-    msg = OscMessageBuilder('/start').build()
+    if value == 0:
+        msg = OscMessageBuilder('/start').build()
+        RPIO.output(LED, True)
+    else:
+        msg = OscMessageBuilder('/stop').build()
+        RPIO.output(LED, False)
     client.send(msg)
-    RPIO.output(LED, True)
-
-
-def stop(gpio_id, value):
-    client = UDPClient(HOST, PORT)
-    msg = OscMessageBuilder('/stop').build()
-    client.send(msg)
-    RPIO.output(LED, False)
 
 
 def main():
-    RPIO.add_interrupt_callback(SWITCH, record, edge='rising',
-                                debounce_timeout_ms=10)
-    RPIO.add_interrupt_callback(SWITCH, stop, edge='falling',
-                                debounce_timeout_ms=10)
+    RPIO.add_interrupt_callback(SWITCH, callback, edge='both')
     RPIO.wait_for_interrupts()
 
 
