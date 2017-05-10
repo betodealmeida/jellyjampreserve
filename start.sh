25e01a80be44b2692ef1b048e3e6d8fdc3b39e2d
Beto De Almeida <roberto@dealmeida.net>
2017-05-09 21:44:03 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# echo commands
+set -x
+date
+
 # start the jack server
 echo "Starting JACK server"
 /usr/local/bin/jackd -R -dalsa -dhw:0 -r48000 -p1024 -n3 &
@@ -8,19 +12,13 @@ echo "Starting JACK server"
 mkdir -p /home/pi/recordings/
 sleep 5
 echo "Starting Timemachine"
-tmux new-session -d -s timemachine 'timemachine -i -t 300 -f wav -p /home/pi/recordings/'
+tmux new-session -d '/usr/local/bin/timemachine -i -t 300 -f wav -p /home/pi/recordings/ system:capture_1 system:capture_2'
 
 # connections
-echo "Waiting before setting up connections..."
-sleep 5
 echo "Connecting system capture to playback"
-jack_connect system:capture_1 system:playback_1
-jack_connect system:capture_2 system:playback_2
-echo "Connecting system capture to Timemachine"
-jack_connect system:capture_1 TimeMachine:in_1
-jack_connect system:capture_2 TimeMachine:in_2
-echo "All connections ready"
-jack_lsp -c
+/usr/local/bin/jack_connect system:capture_1 system:playback_1
+/usr/local/bin/jack_connect system:capture_2 system:playback_2
+/usr/local/bin/jack_lsp -c
 
 echo "Starting JellyJamPreserver"
 python3 /home/pi/jellyjampreserve/jellyjampreserve.py &
